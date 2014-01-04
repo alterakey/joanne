2d5714eadfcbc50d9da6bb10e7b6e0b0d81dda81
Takahiro Yoshimura <altakey@gmail.com>
2014-01-05 03:04:51 +0900

    Testing to create preferences not inflating from somewhere

--- TweetOnAir/src/main/java/com/gmail/altakey/joanne/activity/SettingsChannelActivity.java
+++ TweetOnAir/src/main/java/com/gmail/altakey/joanne/activity/SettingsChannelActivity.java
@@ -1,16 +1,46 @@
 package com.gmail.altakey.joanne.activity;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 
 import com.gmail.altakey.joanne.R;
 import com.gmail.altakey.joanne.fragment.ChannelListFragment;
 
-public class SettingsChannelActivity extends ActionBarActivity {
+public class SettingsChannelActivity extends PreferenceActivity {
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings_channel);
+    protected void onPostCreate(final Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(this);
+        final Preference p = new Preference(this);
+        p.setKey("test_header");
+        p.setTitle("TEST");
+        p.setSummary("test content");
+        p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                final Preference q = new Preference(SettingsChannelActivity.this);
+                q.setKey("test2_header");
+                q.setTitle("TEST2");
+                q.setSummary("test2 content");
+                q.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        screen.removePreference(q);
+                        return true;
+                    }
+                });
+                screen.addPreference(q);
+
+                return true;
+            }
+        });
+
+        screen.addPreference(p);
+        setPreferenceScreen(screen);
     }
 }
